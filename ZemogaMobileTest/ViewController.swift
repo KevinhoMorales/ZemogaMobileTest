f5a6446d807da70d99d4033797242d46be3012ec
Kevin Morales <kfmorales94@gmail.com>
2022-06-26 09:36:00 -0500

    Project init

---
+++ ZemogaMobileTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZemogaMobileTest
+//
+//  Created by Kevinho Morales on 26/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
